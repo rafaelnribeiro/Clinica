a84fd8f172c7c725a698668591663bb64f3f742b
Rafael Ribeiro <rafaelnribeiro@outlook.com>
2018-06-16 16:23:44 -0300

    PacienteDAO Finalizado

--- src/DAO/PacienteDAO.java
+++ src/DAO/PacienteDAO.java
@@ -1,28 +1,34 @@
 package DAO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import entidades.Paciente;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class PacienteDAO extends DAO{
-	
-	public PacienteDAO(String server, String user, String password, int banco) {
+import entidades.Paciente;
+import entidades.Prontuario;
+
+public class PacienteDAO extends DAO {
+
+  public PacienteDAO(String server, String user, String password, int banco) {
     super(server, user, password, banco);
   }
 
   public void insert(Paciente paciente) {
-		try {
-			
-			conectar();
-			
-		StringBuffer buffer = new StringBuffer();
-	     buffer.append("INSERT INTO pessoa (");
+    try {
+
+      conectar();
+
+      StringBuffer buffer = new StringBuffer();
+      buffer.append("INSERT INTO pessoa (");
       buffer.append("cpf, nome, dataNascimento, telefone, email, endereco, sexo, senha");
       buffer.append(") VALUES (");
       buffer.append(retornarValorBDPessoa(paciente));
-      buffer.append(");");      
+      buffer.append(");");
       String sql = buffer.toString();
       comando.executeUpdate(sql);
-      
+
       buffer.setLength(0);
       buffer.append("INSERT INTO paciente (");
       buffer.append("cpfPaciente, tipoSanguineo, peso, altura");
@@ -30,130 +36,252 @@ public class PacienteDAO extends DAO{
       buffer.append(retornarValorBDPaciente(paciente));
       buffer.append(");");
       sql = buffer.toString();
-      comando.executeUpdate(sql);					
-			
-			fechar();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+      comando.executeUpdate(sql);
+
+      fechar();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void remove(Paciente paciente) {
+    try {
+      conectar();
+
+      String sql = ("DELETE FROM PESSOA WHERE cpf= " + retornarValorStringBD(paciente.getCpf()));
+      comando.executeUpdate(sql);
+
+      fechar();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void updatePaciente(Paciente paciente) {
+    try {
+
+      conectar();
+
+      StringBuffer buffer = new StringBuffer();
+      buffer.append("UPDATE PESSOA SET ");
+      buffer.append(retornarCamposBDPessoa(paciente));
+      buffer.append(" WHERE cpf=" + retornarValorStringBD(paciente.getCpf()));
+      String sql = buffer.toString();
+      System.out.println(sql);
+      comando.executeUpdate(sql);
+
+      buffer.setLength(0);
+      buffer.append("UPDATE PACIENTE SET ");
+      buffer.append(retornarCamposBDPaciente(paciente));
+      buffer.append(" WHERE cpfPaciente=" + retornarValorStringBD(paciente.getCpf()));
+      sql = buffer.toString();
+      System.out.println(sql);
+      comando.executeUpdate(sql);
+
+      fechar();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
   
-  	public void remove (Paciente paciente) {
-  		try {
-			conectar();
-			
-			String sql = ("DELETE FROM PESSOA WHERE cpf= " + retornarValorStringBD(paciente.getCpf()));			
-			comando.executeUpdate(sql);			
-			
-			fechar();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-  	}
-  	
-  	public void updatePaciente(Paciente paciente) {
-  		try {
-  			
-  			conectar();
-  			
-  			StringBuffer buffer = new StringBuffer();
-  			buffer.append("UPDATE PESSOA SET ");
-  			buffer.append(retornarCamposBDPessoa(paciente));
-  			buffer.append(" WHERE cpf=" + retornarValorStringBD(paciente.getCpf()));
-  			String sql = buffer.toString();
-  			System.out.println(sql);
-  			comando.executeUpdate(sql);
-  			
+  public Paciente search(String cpf) {
+    try {
+      conectar();
+      
+      String sql = "SELECT * FROM PESSOA WHERE cpf=" + retornarValorStringBD(cpf);
+      ResultSet rs = comando.executeQuery(sql);
+      
+      Paciente paciente = new Paciente();
+      if (rs.next()) {        
+        paciente.setCpf(rs.getString("cpf"));
+        paciente.setNome(rs.getString("nome"));
+        paciente.setData(rs.getDate("dataNascimento"));
+        paciente.setTelefone(rs.getString("telefone"));
+        paciente.setEmail(rs.getString("email"));
+        paciente.setEndereco(rs.getString("endereco"));
+        paciente.setSexo(rs.getString("sexo"));
+        paciente.setSenha(rs.getString("senha"));
+      }
+      
+      sql = "SELECT * FROM PACIENTE WHERE cpfPaciente=" + retornarValorStringBD(cpf);
+      rs = comando.executeQuery(sql);
+      
+      if (rs.next()) {        
+        paciente.setTipoSanguineo(rs.getString("tipoSanguineo"));
+        paciente.setPeso(rs.getDouble("peso"));
+        paciente.setAltura(rs.getDouble("altura"));
+      }
+      
+      sql = "SELECT medicamento FROM ALERGIA_MEDICAMENTOS WHERE cpfPacienteAlergia=" + retornarValorStringBD(cpf);
+      rs = comando.executeQuery(sql);
+      while(rs.next()) {
+        paciente.getMedicamentos().add(rs.getString("medicamento"));
+      }
+      
+      sql = "SELECT doenca FROM DOENÇAS_CRONICAS WHERE cpfPacienteDoenca=" + retornarValorStringBD(cpf);
+      rs = comando.executeQuery(sql);
+      while(rs.next()) {
+        paciente.getDoencasCronicas().add(rs.getString("doenca"));
+      }
+      
+      sql = "SELECT data, hora, ficha FROM PAC_PRONTUARIO WHERE cpfPacienteProntuario=" + retornarValorStringBD(cpf);
+      rs = comando.executeQuery(sql);
+      while(rs.next()) {
+        Prontuario prontuario = new Prontuario();
+        prontuario.setData(rs.getDate("data"));
+        prontuario.setHora(rs.getTime("hora"));
+        prontuario.setFicha(rs.getString("ficha"));       
+        paciente.getProntuario().add(prontuario);
+      }
+      
+      fechar();
+      
+      return paciente;
+      
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e){
+      e.printStackTrace();
+    }
+    return null;
+  }
+  
+  public List<Paciente> retrievePacientes() {
+    try {
+      conectar();
+      
+      String sql = "SELECT * FROM PESSOA";
+      ResultSet rs = comando.executeQuery(sql);
+      
+      List<Paciente> pacientes = new ArrayList<Paciente>();
+      while (rs.next()) {      
+        Paciente paciente = new Paciente();
+        paciente.setCpf(rs.getString("cpf"));
+        paciente.setNome(rs.getString("nome"));
+        paciente.setData(rs.getDate("dataNascimento"));
+        paciente.setTelefone(rs.getString("telefone"));
+        paciente.setEmail(rs.getString("email"));
+        paciente.setEndereco(rs.getString("endereco"));
+        paciente.setSexo(rs.getString("sexo"));
+        paciente.setSenha(rs.getString("senha"));
+        
+        pacientes.add(paciente);
+      }
+      
+      for (Iterator<Paciente> iterator = pacientes.iterator(); iterator.hasNext();) {
+        Paciente paciente = (Paciente) iterator.next();
+        
+        
+        sql = "SELECT * FROM PACIENTE WHERE cpfPaciente="
+            + retornarValorStringBD(paciente.getCpf());
+        rs = comando.executeQuery(sql);
+        if (rs.next()) {        
+          paciente.setTipoSanguineo(rs.getString("tipoSanguineo"));
+          paciente.setPeso(rs.getDouble("peso"));
+          paciente.setAltura(rs.getDouble("altura"));
+        }        
+        
+        sql = "SELECT medicamento FROM ALERGIA_MEDICAMENTOS WHERE cpfPacienteAlergia="
+            + retornarValorStringBD(paciente.getCpf());
+        rs = comando.executeQuery(sql);
+        while(rs.next()) {
+          paciente.getMedicamentos().add(rs.getString("medicamento"));
+        }
+        
+        sql = "SELECT doenca FROM DOENÇAS_CRONICAS WHERE cpfPacienteDoenca="
+            + retornarValorStringBD(paciente.getCpf());
+        rs = comando.executeQuery(sql);
+        while(rs.next()) {
+          paciente.getDoencasCronicas().add(rs.getString("doenca"));
+        }
+        
+        sql = "SELECT data, hora, ficha FROM PAC_PRONTUARIO WHERE cpfPacienteProntuario="
+            + retornarValorStringBD(paciente.getCpf());
+        rs = comando.executeQuery(sql);
+        
+        if(rs.next()) {
+          Prontuario prontuario = new Prontuario();
+          prontuario.setData(rs.getDate("data"));
+          prontuario.setHora(rs.getTime("hora"));
+          prontuario.setFicha(rs.getString("ficha"));       
+          paciente.getProntuario().add(prontuario);
+        } 
+        
+      }
+      
+      sql = "SELECT * FROM PACIENTE";
+      rs = comando.executeQuery(sql);      
+      
+      
+      fechar();
+      
+      return pacientes;
+      
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e){
+      e.printStackTrace();
+    }
+    return null;
+  }
+  
+  private String retornarValorBDPaciente(Paciente paciente) {
+    return retornarValorStringBD(paciente.getCpf()) + ", " + retornarValorStringBD(paciente.getTipoSanguineo()) + ", "
+        + retornarValorStringBD(Double.toString(paciente.getPeso())) + ", "
+        + retornarValorStringBD(Double.toString(paciente.getAltura()));
+  }
 
-  			buffer.setLength(0);
-  			buffer.append("UPDATE PACIENTE SET ");
-  			buffer.append(retornarCamposBDPaciente(paciente));
-  			buffer.append(" WHERE cpfPaciente=" + retornarValorStringBD(paciente.getCpf()));
-  			sql = buffer.toString();
-  			System.out.println(sql);
-  			comando.executeUpdate(sql);  			
-  			
-  			
-  			fechar();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-  	}
+  private String retornarValorBDPessoa(Paciente paciente) {
+    return retornarValorStringBD(paciente.getCpf()) + ", " + retornarValorStringBD(paciente.getNome()) + ", "
+        + retornarValorStringBD(paciente.getData().toString()) + ", " + retornarValorStringBD(paciente.getTelefone())
+        + ", " + retornarValorStringBD(paciente.getEmail()) + ", " + retornarValorStringBD(paciente.getEndereco())
+        + ", " + retornarValorStringBD(paciente.getSexo()) + ", " + retornarValorStringBD(paciente.getSenha());
+  }
 
-	private String retornarValorBDPaciente(Paciente paciente) {
-		return
-		    retornarValorStringBD(paciente.getCpf())
-		    + ", " +
-        retornarValorStringBD(paciente.getTipoSanguineo())
-        + ", " +
-        retornarValorStringBD(Double.toString(paciente.getPeso()))
-        + ", " +
-        retornarValorStringBD(Double.toString(paciente.getAltura()));
-	}
-	
-	 private String retornarValorBDPessoa(Paciente paciente) {
-	    return
-	        retornarValorStringBD(paciente.getCpf())
-	        + ", " +
-	        retornarValorStringBD(paciente.getNome())
-	        + ", " +
-	        retornarValorStringBD(paciente.getData().toString())
-	        + ", " +
-	        retornarValorStringBD(paciente.getTelefone())
-	        + ", " +
-	        retornarValorStringBD(paciente.getEmail())
-	        + ", " + 
-	        retornarValorStringBD(paciente.getEndereco())
-	        + ", " + 
-	        retornarValorStringBD(paciente.getSexo())
-	        + ", " +
-	        retornarValorStringBD(paciente.getSenha());
-	  }
-	 
-	 private String retornarCamposBDPessoa(Paciente paciente) {
-		 StringBuffer buffer = new StringBuffer();
-		 buffer.append("cpf= ");
-		 buffer.append(retornarValorStringBD(paciente.getCpf()));
-		 buffer.append(", nome= ");
-		 buffer.append(retornarValorStringBD(paciente.getNome()));
-		 buffer.append(", dataNascimento= ");
-		 buffer.append(retornarValorStringBD(paciente.getData().toString()));
-		 buffer.append(", telefone= ");
-		 buffer.append(retornarValorStringBD(paciente.getTelefone()));
-		 buffer.append(", email= ");
-		 buffer.append(retornarValorStringBD(paciente.getEmail()));
-		 buffer.append(", endereco= ");
-		 buffer.append(retornarValorStringBD(paciente.getEndereco()));
-		 buffer.append(", sexo= ");
-		 buffer.append(retornarValorStringBD(paciente.getSexo()));
-		 buffer.append(", senha= ");
-		 buffer.append(retornarValorStringBD(paciente.getSenha()));
-		 
-		 return buffer.toString();
-		 
-	 }
-	 
-	 private String retornarCamposBDPaciente(Paciente paciente) {
-		 StringBuffer buffer = new StringBuffer();
-		 buffer.append("cpfPaciente= ");
-		 buffer.append(retornarValorStringBD(paciente.getCpf()));
-		 buffer.append(", tipoSanguineo= ");
-		 buffer.append(retornarValorStringBD(paciente.getTipoSanguineo()));
-		 buffer.append(", peso= ");
-		 buffer.append(Double.toString(paciente.getPeso()));
-		 buffer.append(", altura= ");
-		 buffer.append(Double.toString(paciente.getAltura()));
-		 
-		 return buffer.toString();
-		 
-	 }
-	
+  private String retornarCamposBDPessoa(Paciente paciente) {
+    StringBuffer buffer = new StringBuffer();
+    buffer.append("cpf= ");
+    buffer.append(retornarValorStringBD(paciente.getCpf()));
+    buffer.append(", nome= ");
+    buffer.append(retornarValorStringBD(paciente.getNome()));
+    buffer.append(", dataNascimento= ");
+    buffer.append(retornarValorStringBD(paciente.getData().toString()));
+    buffer.append(", telefone= ");
+    buffer.append(retornarValorStringBD(paciente.getTelefone()));
+    buffer.append(", email= ");
+    buffer.append(retornarValorStringBD(paciente.getEmail()));
+    buffer.append(", endereco= ");
+    buffer.append(retornarValorStringBD(paciente.getEndereco()));
+    buffer.append(", sexo= ");
+    buffer.append(retornarValorStringBD(paciente.getSexo()));
+    buffer.append(", senha= ");
+    buffer.append(retornarValorStringBD(paciente.getSenha()));
+
+    return buffer.toString();
+
+  }
+
+  private String retornarCamposBDPaciente(Paciente paciente) {
+    StringBuffer buffer = new StringBuffer();
+    buffer.append("cpfPaciente= ");
+    buffer.append(retornarValorStringBD(paciente.getCpf()));
+    buffer.append(", tipoSanguineo= ");
+    buffer.append(retornarValorStringBD(paciente.getTipoSanguineo()));
+    buffer.append(", peso= ");
+    buffer.append(Double.toString(paciente.getPeso()));
+    buffer.append(", altura= ");
+    buffer.append(Double.toString(paciente.getAltura()));
+
+    return buffer.toString();
+
+  }
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import DAO.PacienteDAO;
 import conexao.ConFactory;
@@ -14,15 +17,32 @@ public class Main {
 		
 		Paciente p1 = new Paciente("12345678901", "Fulano", data, "(84)7894-9874", 
 				"fulano@gmail.com", "Rua das Oliveiras", "M", "12345", "O+", 50, 1.5);
+		Paciente p2 = new Paciente("99999999999", "Sicrano", data, "(84)9512-9874", 
+        "sicrano@gmail.com", "Rua das Trincheiras", "M", "12345", "O+", 50, 1.5);
+		Paciente p3 = new Paciente("77777777777", "Beltrano", data, "(84)3578-9874", 
+        "beltrano@gmail.com", "Rua das Torneiras", "M", "12345", "O+", 50, 1.5);
 		
 		//PacienteDAO pDAO = new PacienteDAO("jdbc:mysql://localhost/clinica", "aluno", "aluno", ConFactory.MYSQL);
 		PacienteDAO pDAO = new PacienteDAO("jdbc:mysql://localhost/clinica", "root", "root", ConFactory.MYSQL);
 		
-		//pDAO.insert(p1);
-		//p1.setNome("Joaquim");
-		//p1.setAltura(1.75);
-	  //pDAO.updatePaciente(p1);
+/*	pDAO.insert(p1);
+		pDAO.insert(p2);
+		pDAO.insert(p3);*/
+		
+/*	p1.setNome("Joaquim");
+		p1.setAltura(1.75);
+	  pDAO.updatePaciente(p1);*/
+		
 	  //pDAO.remove(p1);
+		
+		Paciente p4 = pDAO.search("12345678901");
+		System.out.println(p4.getNome() + " " +p4.getAltura());
+		
+/*		List<Paciente> pacientes = pDAO.retrievePacientes();		
+		for (Iterator iterator = pacientes.iterator(); iterator.hasNext();) {
+      Paciente paciente = (Paciente) iterator.next();
+      System.out.println(paciente.getNome() + " " +paciente.getAltura());
+    }*/
 	}
 
 }
